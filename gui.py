1545f562407c6072c1cb7df0b0b3992b99467b41
Troy Mustang <ajayiyer10@gmail.com>
2025-04-21 10:34:52 +0530

    Refactor driver data structure and update GUI to Streamlit

--- gui.py
+++ gui.py
@@ -1,24 +1,5 @@
-import customtkinter
-from customtkinter import *
-from my_package import driver_data
+import streamlit as st
 
-app = CTk()
-app.title("Formula Uno")
-app.geometry("600x650")
+st.title('FormulaUno')
 
-def clicker():
-    my_progressbar.step()
-    my_label.configure(text=(int(my_progressbar.get()*100)))
-
-my_progressbar = CTkProgressBar(app, orientation="horizontal")
-my_progressbar.pack(pady=40)
-my_progressbar.set(0)
-
-my_button = CTkButton(app, text="Click Me", command=clicker)
-my_button.pack(pady=20)
-
-my_label = CTkLabel(app, text='')
-my_label.pack(pady=20)
-
-app.mainloop()
-
+st.text('An F1 strategy app')
